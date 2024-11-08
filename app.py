dc5b094811e60535ac8e6c15fca0df39512bc587
cvr-devops <venkat.chittipeddi01@gmail.com>
2024-11-08 16:22:16 +0530

    crack it

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
 
-    return "Happy Diwali india,have a blasting day !"
+    return "Happy Diwali india,have a blasting day the fire crackers! and be safe"
 
 if __name__ == '__main__':
     app.run(debug=True)
