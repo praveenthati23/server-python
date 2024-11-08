471dd96ffd95baa5e3820ea23c3199e405a1c4ad
cvr-devops <venkat.chittipeddi01@gmail.com>
2024-11-08 14:59:55 +0530

    first commit

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Happy Diwali"
+    return "Happy Diwali to you praveenkumar thati"
 
 if __name__ == '__main__':
     app.run(debug=True)
